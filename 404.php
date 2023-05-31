562db60a8852f03406e91e53f74c5f9de4029526
Hong Phuc <hongphuc.dthp@gmail.com>
2023-05-31 23:34:34 +0700

    followandrew - create custom wp theme

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php
+get_header();
+?>
+
+<section id="not-found">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 d-flex flex-column justify-content-center intro-text">
+                <p class="my-4">
+                    <strong>Oops! The page you're looking for cannot be found.</strong> It's possible that the page has been removed or the link is incorrect.
+                </p>
+            </div>
+            <div class="col-6">
+                <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_4qhciwpm.json" background="transparent" speed="1" style="width: auto; height: auto" loop autoplay></lottie-player>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php
+get_footer();
+?>
