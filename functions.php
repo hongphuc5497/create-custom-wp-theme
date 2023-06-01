9e556ef75bf46ec2946b691d9d7a3cf0a39be016
Hong Phuc <hongphuc.dthp@gmail.com>
2023-06-01 23:03:14 +0700

    followandrew - add images and dynamic title support

--- functions.php
+++ functions.php
@@ -1,17 +1,31 @@
 <?php
 
-function followandrew_register_style(){
-
-	$version = wp_get_theme()->get('Version');
-	wp_enqueue_style('followandrew-style', get_template_directory_uri() . "/style.css", array('followandrew-bootstrap'), $version, 'all');
-
-	wp_enqueue_style('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
-
-	wp_enqueue_style('followandrew-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
-
-
+function followandrew_theme_support(){
+	// Add dynamic title tag support
+	add_theme_support('title-tag');
 }
 
-add_action('wp_enqueue_scripts', 'followandrew_register_style');
+add_action('after_setup_theme', 'followandrew_theme_support');
+
+function followandrew_register_styles(){
+	$version = wp_get_theme()->get('Version');
+	wp_enqueue_style('followandrew-style', get_template_directory_uri() . '/style.css', array('followandrew-bootstrap'), $version, 'all');
+
+	wp_enqueue_style('followandrew-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', array(), '4.4.1', 'all');
+	wp_enqueue_style('followandrew-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css', array(), '5.13.0', 'all');
+}
+
+add_action('wp_enqueue_scripts', 'followandrew_register_styles');
+
+function followandrew_register_scripts(){
+	$version = wp_get_theme()->get('Version');
+	wp_enqueue_script('followandrew-main', get_template_directory_uri() . '/assets/js/main.css', array(), $version, 'all');
+
+	wp_enqueue_script('followandrew-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
+	wp_enqueue_script('followandrew-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
+	wp_enqueue_script('followandrew-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true);
+}
+
+add_action('wp_enqueue_scripts', 'followandrew_register_scripts');
 
 ?>
